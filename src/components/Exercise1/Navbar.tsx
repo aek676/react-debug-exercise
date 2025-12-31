f29ce33ec30203024ba03ee7f5aa11e4d06f8aeb
aek676 <aek676@inlumine.ual.es>
2026-01-01 00:22:50 +0100

    style: standardize code formatting and improve readability across components

--- src/components/Exercise1/Navbar.tsx
+++ src/components/Exercise1/Navbar.tsx
@@ -1,26 +1,29 @@
-import { useState } from "react";
-import { Button } from "../ui/button";
+import { useState } from 'react';
+import { Button } from '../ui/button';
 
 interface NavbarProps {
-    isLoginedIn: boolean;
-    handleLogout: () => void;
+  isLoginedIn: boolean;
+  handleLogout: () => void;
 }
 
 export default function Navbar({ isLoginedIn, handleLogout }: NavbarProps) {
-    const handleLogoutButton = () => {
-        handleLogout();
-    }
+  const handleLogoutButton = () => {
+    handleLogout();
+  };
 
-    return (
-        <div className="bg-gray-400 color-[#333] h-20 p-5">
-            <div className="flex justify-between">
-                <div className="text-[1.8em]">Exercise 1</div>
-                {isLoginedIn && (
-                    <Button className="color-[#333] border-gray-400" onClick={handleLogoutButton}>
-                        Sign Out
-                    </Button>
-                )}
-            </div>
-        </div>
-    )
-};
+  return (
+    <div className="bg-gray-400 color-[#333] h-20 p-5">
+      <div className="flex justify-between">
+        <div className="text-[1.8em]">Exercise 1</div>
+        {isLoginedIn && (
+          <Button
+            className="color-[#333] border-gray-400"
+            onClick={handleLogoutButton}
+          >
+            Sign Out
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
